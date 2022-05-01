f140514d40b991ab1eaecc3a174c6c7c835a48f1
Crowdbotics <team@crowdbotics.com>
2022-05-01 12:00:58 +0000

    Update components

---
+++ screens/Frame1(1:2)2278797_1_2/index.js
@@ -0,0 +1,100 @@
+import React from "react"
+import {
+  View,
+  Image,
+  ImageBackground,
+  TouchableOpacity,
+  Text,
+  Button,
+  Switch,
+  TextInput,
+  StyleSheet,
+  ScrollView
+} from "react-native"
+import Icon from "react-native-vector-icons/FontAwesome"
+import { CheckBox } from "react-native-elements"
+import { connect } from "react-redux"
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp
+} from "react-native-responsive-screen"
+import { getNavigationScreen } from "@screens"
+export class Blank extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {}
+  }
+  render = () => (
+    <ScrollView
+      contentContainerStyle={{ flexGrow: 1 }}
+      style={styles.ScrollView_1}
+    >
+      <View style={styles.View_2} />
+      <View style={styles.View_1_3}>
+        <Text style={styles.Text_1_3}>Messages</Text>
+      </View>
+      <ImageBackground
+        source={{
+          uri:
+            "https://s3-us-west-2.amazonaws.com/figma-alpha-api/img/b99c/3850/4ab333c8ab03880cff2b887d5b2a2215"
+        }}
+        style={styles.ImageBackground_1_9}
+      />
+      <View style={styles.View_1_10} />
+    </ScrollView>
+  )
+}
+
+const styles = StyleSheet.create({
+  ScrollView_1: { backgroundColor: "rgba(255, 255, 255, 1)" },
+  View_2: { height: hp("100%") },
+  View_1_3: {
+    width: wp("18%"),
+    minWidth: wp("18%"),
+    minHeight: hp("3%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("41%"),
+    top: hp("2%"),
+    justifyContent: "flex-start"
+  },
+  Text_1_3: {
+    color: "rgba(0, 0, 0, 1)",
+    fontSize: 13,
+    fontWeight: "700",
+    textAlign: "left",
+    fontStyle: "normal",
+    letterSpacing: 0,
+    textTransform: "none"
+  },
+  ImageBackground_1_9: {
+    width: wp("3%"),
+    height: hp("3%"),
+    top: hp("2%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("4%")
+  },
+  View_1_10: {
+    width: wp("18%"),
+    minWidth: wp("18%"),
+    height: hp("11%"),
+    minHeight: hp("11%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("7%"),
+    top: hp("10%")
+  }
+})
+
+const mapStateToProps = state => {
+  return {}
+}
+const mapDispatchToProps = () => {
+  return {}
+}
+export default connect(mapStateToProps, mapDispatchToProps)(Blank)
